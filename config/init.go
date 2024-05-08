29ab549a53c289d08ac47f14f0a9db7df5f54167
Hùng Nguyễn <nguyenduyhung04092004@gmail.com>
2024-05-08 11:25:42 +0700

    c

--- config/init.go
+++ config/init.go
@@ -2,16 +2,22 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/go-chi/jwtauth/v5"
 )
 
 func init() {
+	loadEnv()
+
 	var migrate bool = false
 	flag.BoolVar(&migrate, "db", true, "Migrate Database?")
+	var mongoDBURL string
+	flag.StringVar(&mongoDBURL, "host", "localhost", "")
+	flag.Parse()
 
-	loadEnv()
+	mongodbLocal = fmt.Sprintf("mongodb://%s:27017", mongoDBURL)
 	jwt = jwtauth.New("HS256", []byte("h-shop"), nil)
 
 	if err := connectMongoDB(migrate); err != nil {
